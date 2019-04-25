fed18f70ec8306d2196197016f267b4538c33d15
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-25 15:21:39 -0400

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Clicky Game -React
+        Soccer Game
       </a>
     
 
