e1fa9a3249d844f584dad4a0ba7065ba022287d5
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-28 10:34:46 -0400

    Updates

---
+++ precache-manifest.d8e7b06e4e2e2604322a9217e293cfed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65bbf04337ae7d31b205340a2d746d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27a3c84495d9f3520d4",
+    "url": "/static/css/main.65f799c3.chunk.css"
+  },
+  {
+    "revision": "04a85e2ef7c44591185a",
+    "url": "/static/js/2.ad373512.chunk.js"
+  },
+  {
+    "revision": "c27a3c84495d9f3520d4",
+    "url": "/static/js/main.4a2a4686.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
